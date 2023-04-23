254b9bf536ed705f628c8b45882b83b75a4cb105
khush narang <khushnarang@khushs-MacBook-Air.local>
2023-04-23 22:48:08 +0530

    updated

--- src/components/NavPages/Schedule.jsx
+++ src/components/NavPages/Schedule.jsx
@@ -1,26 +1,34 @@
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import "../allStyles/Schedule.css";
+import calander from "../calander.gif";
 
 function Schedule (){
     return (
         <>
             <Navbar />
             <div className="dummy"></div>
+
             <div className="mainScheduler">
+                <div className="caldetails">
+                    <img src={calander} className='calander'/>
+                </div>
                 <div className="details">
                     <form action="">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" name="" id="" />
-                        <label htmlFor="email">Subject</label>
-                        <input type="text" name="" id="" />
+                        <div className="top1">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="" id="" />
+                        </div>
+                        <div className="top2">
+                            <label htmlFor="email">Subject</label>
+                            <input type="text" name="" id="" />
+                        </div>
                         <div className="clockSet">
                             <input type="datetime-local" name="" id="" max={`${(new Date(Date.now() + 2 * 24 * 60 * 60 * 1000)).toISOString().slice(0, -8)}`} />
                         </div>
-                        
+                        <input className="formButton" type="submit" value="Send"/>
                     </form>
-                </div>
-                
+                </div>   
             </div>
             <Footer />
         </>
