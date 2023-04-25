34a8aac522a9efe0c7a0f60886fe921d830cca4b
khush narang <khushnarang@khushs-MacBook-Air.local>
2023-04-25 17:15:23 +0530

    updated

--- src/components/NavPages/Home.jsx
+++ src/components/NavPages/Home.jsx
@@ -17,7 +17,7 @@ function Home (){
     function generateRandomString() {
         const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         let result = '';
-        for (let i = 0; i < 6; i++) {
+        for (let i = 0; i < 10; i++) {
           result += chars[Math.floor(Math.random() * chars.length)];
         }
         return result;
